67bb27afc8eb30fe5528b6c7b9d4b1b9efcd62dc
sheetalbeck01 <sheetalbeck01@gmail.com>
2026-09-11 19:13:35 +0530

    delete method

--- books.py
+++ books.py
@@ -64,3 +64,12 @@ def update_book(update_book=Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
             BOOKS[i] = update_book
+
+
+# Deleting Book
+@app.delete("/books/delete_book/{book_title}")
+def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
